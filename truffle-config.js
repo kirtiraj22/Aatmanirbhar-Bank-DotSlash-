26aed7a446aed48e8da3df5ddd034b9e7ff45b05
kirtiraj22 <kirtirajpatanwadia064@gmail.com>
2023-02-19 10:49:15 +0530

    updation in dotenv and deployment

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,26 @@
 const path = require("path");
 
+require('dotenv').config();
+const {MNEMONIC , PROJECT_ID} = process.env
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "5777", // Match any network id
-    },
+    // development: {
+    //   host: "127.0.0.1",
+    //   port: 7545,
+    //   network_id: "5777", // Match any network id
+    // },
+    goerli: {
+      provider: () => new HDWalletProvider(MNEMONIC, `wss://eth-goerli.g.alchemy.com/v2/${PROJECT_ID}`),
+      network_id: '5',
+      confirmations: 2,
+      timeoutBlocks: 2000,
+      skipDryRun: true
+    }
   },
 };
